4fd40559b30d9f79a6d60090290752f4115f781c
Eric Van Dyn Hovne <eric.vandynhoven@gmail.com>
2017-01-16 12:33:27 -0800

    created control flow logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,17 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert ("Sorry, try again.")
+} else if (cardTwo === cardThree) {
+	alert ("Sorry, try again.")
+} else if (cardTwo === cardOne) {
+	alert ("You found a match!")
+} else if (cardOne === cardFour) {
+	alert ("Sorry, try again.")
+} else if (cardOne === cardThree) {
+	alert ("Sorry, try again.")
+} else {
+	alert ("You found a match!")
+}
