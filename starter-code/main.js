cdedc94584828df982bc0966c1f967f19fec07e3
Eric Van Dyn Hovne <eric.vandynhoven@gmail.com>
2017-01-18 23:21:20 -0800

    created full funtionality to cards and gamelogic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,13 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "king";
+var cardFour = "king";*/
 
-/*if (cardTwo === cardFour) {
+
+/*
+if (cardTwo === cardFour) {
 	alert ("Sorry, try again.")
 } else if (cardTwo === cardThree) {
 	alert ("Sorry, try again.")
@@ -17,7 +19,8 @@ var cardFour = "king";
 	alert ("Sorry, try again.")
 } else {
 	alert ("You found a match!")
-}*/
+}
+
 
 var gameBoard = document.getElementById('game-board');
 
@@ -29,3 +32,52 @@ var createCards = function(num) {
 	}
 };
 createCards(4);
+*/
+
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
+
+var gameBoard = document.getElementById('game-board');
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+		gameBoard.appendChild(cardElement);
+	}
+};
+
+var isMatch = function(cards) {
+	if (cards[0] === cards[1]) {
+		alert ("You found a match!");
+	} else {
+		alert ('Sorry, try again.');
+	}
+	resetImage(cards);
+};
+
+var isTwoCards = function() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='images/diamonds-884195_1280.png'>";
+	} else {
+		this.innerHTML = "<img src='images/clubs-884198_1280.png'>";
+	}
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+	
+};	
+
+var resetImage = function(cards) {
+	for (var i = 0; i < cards.length; i++) {
+		cards[i].innerHTML = "<p><p/>";
+	}
+}
+
+createBoard();
+
